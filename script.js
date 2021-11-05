8fd96069e4df61719c4063e9f0d52656f075b4cc
Максим Горкольцев <vundey@mail2000.ru>
2021-11-05 21:05:21 +0700

    first json

---
+++ script.js
@@ -0,0 +1,47 @@
+let file = document.querySelector(".file");
+let btn = document.querySelector(".btn");
+
+let title = document.querySelector(".title");
+let form = document.querySelector(".form");
+
+
+
+function readerJSON(){
+    let test = file.files[0];
+
+    let reader = new FileReader();
+    reader.readAsText(test);
+    reader.onload = function(){
+        // Добавление заголовка
+        let text = reader.result;
+        const json = JSON.parse(text);
+        title.textContent = json.name;
+        // Добавление формы с импутами
+        for(let i=0; i <= json.fields.length; i++){
+            let el = document.createElement("label");
+            let el2 = document.createElement("input");
+    
+            el.textContent = json.fields[i].label;
+            el2.type = json.fields[i].input.type;
+            el2.required = json.fields[i].input.required;
+    
+            form.appendChild(el);
+            form.appendChild(el2);
+        }
+    
+        if(!json.references){
+            let el3 = document.createElement("label");
+        }
+    
+        file.disabled = true;
+    }
+}
+
+function btnEvent(){
+    title.textContent = null;
+    file.disabled = false;
+    form.remove();
+}
+
+file.addEventListener("change", readerJSON, false);
+btn.addEventListener("click", btnEvent, false);
